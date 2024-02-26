dbeb9e945307279b5cd4989a1bb68bb663887bb6
Rahul Bajaj <63620041+rahul0eth@users.noreply.github.com>
2024-02-26 11:01:56 -0700

    my portfolio

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,67 +1,89 @@
 export const profile = {
-  name: "Sero",
+  name: "Rahul",
+  ens: "rahul0.eth",
   role: "Software Engineer",
-  bio: "I'm a software engineer that loves building products and teaching others how to do the same.",
-  imageUrl: "https://i.imgur.com/2m4XIqN.png",
-  address: "0xc0163E58648b247c143023CFB26C2BAA42C9d9A9",
+  bio: "I'm an aspiring software engineer who loves crypto development.",
+  //imageUrl: "https://i.imgur.com/2m4XIqN.png",
+  imageUrl: "https://i.imgur.com/YCCdTU8.png",
+  address: "0xa2ae42a1b1977120ac4f24018becadcd4bcf7c09",
   links: [
     {
       name: "Twitter",
-      url: "https://twitter.com/seroxdesigns",
+      url: "https://twitter.com/rahul0_eth",
       icon: "twitter",
-    },
+    }
+    ,
+    {
+      name: "Telegram",
+      url: "https://t.me/rahul0_ethh",
+      icon: "telegram",
+    }
+    ,
     {
       name: "GitHub",
-      url: "https://github.com/Seroxdesign",
-      icon: "",
-    },
+      url: "https://github.com/rahul0eth",
+      icon: "github",
+    }
+    ,
     {
       name: "LinkedIn",
-      url: "https://www.linkedin.com/in/sero-346b85202/",
-      icon: "",
-    },
-    {
-      name: "Blog",
-      url: "https://blog.serotonindesigns.com",
-      icon: "",
-    },
-    {
-      name: 'Podcast',
-      url: 'https://open.spotify.com/show/2HfqI2dFN85uLLFwGqEk0d?si=f918a9d9ea6f4d38',
-      icon: ''
+      url: "https://www.linkedin.com/in/rahul0eth/",
+      icon: "linkedin",
     }
+    ,
+    {
+      name: "Warpcast",
+      url: "https://warpcast.com/rahulb",
+      icon: "warpcast",
+    }
+    // ,
+    // {
+    //   name: "Blog",
+    //   url: "https://blog.serotonindesigns.com",
+    //   icon: "",
+    // }
+    // ,
+    // {
+    //   name: 'Podcast',
+    //   url: 'https://open.spotify.com/show/2HfqI2dFN85uLLFwGqEk0d?si=f918a9d9ea6f4d38',
+    //   icon: ''
+    // }
   ],
   projects: [
     {
-      name: 'ETH DENVER BUIDL REPO',
-      description: 'My work at the ETH Denver hackathon',
+      name: 'xSafe',
+      description: 'Deploy to the same address on all chains. Forget nonces and salts.',
       imageUrl: '',
-      url: 'https://github.com/Seroxdesign/buidl-portfolio'
-    },
-    {
-      name: 'Superfluid',
-      description: '',
-      imageUrl: '',
-      url: 'https://console.superfluid.finance'
-    },
-    {
-      name: 'Tellor',
-      description: '',
-      imageUrl: '',
-      url: 'https://tellor.io'
-    },
-    {
-      name: 'Pokt',
-      description: '',
-      imageUrl: '',
-      url: 'https://pokt.network'
-    },
-    {
-      name: 'Gitcoin',
-      description: '',
-      imageUrl: '',
-      url: 'https://gitcoin.co'
+      url: 'https://xsafe.chainrule.io/'
     }
+    ,
+    {
+      name: 'xPay',
+      description: 'Pay once, execute everywhere.',
+      imageUrl: '',
+      url: 'https://xpay-ui.vercel.app/'
+    }
+    // ,
+    // {
+    //   name: 'Tellor',
+    //   description: '',
+    //   imageUrl: '',
+    //   url: 'https://tellor.io'
+    // }
+    // ,
+    // {
+    //   name: 'Pokt',
+    //   description: '',
+    //   imageUrl: '',
+    //   url: 'https://pokt.network'
+    // }
+    // ,
+    // {
+    //   name: 'Gitcoin',
+    //   description: '',
+    //   imageUrl: '',
+    //   url: 'https://gitcoin.co'
+    // }
 
   ]
 }
